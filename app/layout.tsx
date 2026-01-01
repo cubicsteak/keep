0bfdbfd63c180225b59b2ded06ba13fa4dbceb47
Brad <4bigchoi23@gmail.com>
2026-01-01 22:27:24 +0900

    feat: migrate google fonts to next/font for optimization

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,44 @@ export const metadata: Metadata = {
   description: "Keep your bookmark!",
 };
 
+import {
+  Noto_Sans_KR,
+  Noto_Serif_KR,
+  Montserrat,
+  Do_Hyeon,
+  Geist,
+  Geist_Mono,
+} from 'next/font/google';
+
+const notoSansKr = Noto_Sans_KR({
+  subsets: ['latin'],
+  weight: ['100', '400', '700', '900'],
+  variable: '--font-noto-sans',
+});
+const notoSerifKr = Noto_Serif_KR({
+  subsets: ['latin'],
+  weight: ['200', '400', '700', '900'],
+  variable: '--font-noto-serif',
+});
+const geist = Geist({
+  subsets: ['latin'],
+  variable: '--font-geist',
+});
+const geistMono = Geist_Mono({
+  subsets: ['latin'],
+  variable: '--font-geist-mono',
+});
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: ['100', '400', '700', '900'],
+  variable: '--font-montserrat',
+});
+const doHyeon = Do_Hyeon({
+  weight: '400',
+  subsets: ['latin'],
+  variable: '--font-do-hyeon',
+});
+
 import "./globals.css";
 import { ThemeProvider } from "@/components/app/theme-provider"
 import { Toaster } from "@/components/ui/sonner"
@@ -14,7 +52,17 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html 
+      lang="ko" 
+      className={[
+        notoSansKr.variable,
+        notoSerifKr.variable,
+        geist.variable,
+        geistMono.variable,
+        montserrat.variable,
+        doHyeon.variable
+      ].join(' ')} 
+      suppressHydrationWarning>
       <body>
         <ThemeProvider
           attribute="class"
