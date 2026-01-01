cd601006434bfc66ef8acd82c7b5e0975faffc31
Brad <4bigchoi23@gmail.com>
2026-01-01 21:18:48 +0900

    fix: export middleware as a function

--- auth.config.ts
+++ auth.config.ts
@@ -1,27 +1,5 @@
-import Google from "next-auth/providers/google"
-import GitHub from "next-auth/providers/github"
-import Nodemailer from "next-auth/providers/nodemailer"
 import type { NextAuthConfig } from "next-auth"
 
-const config: NextAuthConfig = {
-  providers: [
-    Google,
-    GitHub,
-    Nodemailer({
-      server: process.env.EMAIL_SERVER,
-      from: process.env.EMAIL_FROM,
-    }),
-  ],
-  pages: {
-    signIn: "/auth/signin",
-    error: "/auth/error",
-    verifyRequest: "/auth/verify-request",
-  },
-  session: { strategy: "jwt" },
-  theme: {
-    colorScheme: "dark",
-    logo: "https://authjs.dev/img/logo-sm.png",
-  },
-}
-
-export default config
+export default {
+  providers: [],
+} satisfies NextAuthConfig
--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,6 @@
-export { auth as middleware } from "./auth"
+import NextAuth from "next-auth"
+import authConfig from "./auth.config"
+
+// export const { auth: middleware } = NextAuth(authConfig)
+export const { auth } = NextAuth(authConfig)
+export { auth as middleware }
